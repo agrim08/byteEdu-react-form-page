3ff3196f198def4feea1b17941ca48f68a99347b
agrim08 <agrimgupta0805@gmail.com>
2024-11-04 19:11:47 +0530

    chnages

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAm5v-hfhNjE9DGWEB8yqpJfP_Q4a4eb2o",
-  authDomain: "todoapp-eeeb7.firebaseapp.com",
-  projectId: "todoapp-eeeb7",
-  storageBucket: "todoapp-eeeb7.appspot.com",
-  messagingSenderId: "1072574112522",
-  appId: "1:1072574112522:web:65fc4e184aed9894dc90f3",
+  apiKey: "AIzaSyAieuIup69Dx-cq-mPbIFNHMgRGgqKkZv0",
+  authDomain: "byteedu-155c6.firebaseapp.com",
+  projectId: "byteedu-155c6",
+  storageBucket: "byteedu-155c6.firebasestorage.app",
+  messagingSenderId: "637902668862",
+  appId: "1:637902668862:web:cfa9abdd80157608e8fd6f",
+  measurementId: "G-1S8E3WSCQV",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export default app;
